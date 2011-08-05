844e54f10996a5cb36de61f47eb815bb1f158bd5
Douglas Meehan <dmeehan@gmail.com>
2011-08-05 11:45:13 -0400

    fixed installed apps

--- futuregreen/settings.py
+++ futuregreen/settings.py
@@ -41,7 +41,7 @@ STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static'),
+    # add additional static file directories here
 )
 
 STATICFILES_FINDERS = (
