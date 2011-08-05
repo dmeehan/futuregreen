bfca82030c7ff7af01a508c83d6ffb3bbb4900a8
Douglas Meehan <dmeehan@gmail.com>
2011-08-05 11:03:25 -0400

    modified installed apps

--- futuregreen/settings.py
+++ futuregreen/settings.py
@@ -100,14 +100,17 @@ MIDDLEWARE_CLASSES = (
 #==============================================================================
 
 INSTALLED_APPS = (
+    # admin
+    'grappelli',
+    'django.contrib.admin',
+    'django.contrib.admindocs'
+
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'django.contrib.admindocs',
 
     # third party apps
     'south',
