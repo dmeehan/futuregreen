71afd9f3e6bfbed565d29983b19f97037cc9e2f8
Douglas Meehan <dmeehan@gmail.com>
2011-08-09 18:11:25 -0400

    work in progress

--- futuregreen/portfolio/admin.py
+++ futuregreen/portfolio/admin.py
@@ -6,19 +6,35 @@ from futuregreen.portfolio.models import Project
 
 class ClientInline(admin.TabularInline):
     model = Project.clients.through
+    verbose_name = 'client'
+    verbose_name_plural = 'clients'
 
 class CollaboratorInline(admin.TabularInline):
     model = Project.collaborators.through
+    verbose_name = 'collaborator'
+    verbose_name_plural = 'collaborators'
+    extra=0
 
 class DesignerInline(admin.TabularInline):
     model = Project.designers.through
+    verbose_name = 'designer'
+    verbose_name_plural = 'designers'
+    extra=0
+
+class CategoryInline(admin.TabularInline):
+    model = Project.categories.through
+    verbose_name = 'category'
+    verbose_name_plural = 'categories'
+    extra=0
 
 class ProjectAdmin(admin.ModelAdmin):
    inlines = [
        ClientInline,
        CollaboratorInline,
        DesignerInline,
+       CategoryInline,
    ]
+   exclude = ('clients', 'collaborators', 'employees', 'categories',)
    list_display = ('name',)
    prepopulated_fields = {"slug": ("name",)}
 
