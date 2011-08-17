3ab422f023ebb13a6010cf7eacd7198aff8f78ab
Douglas Meehan <dmeehan@gmail.com>
2011-08-17 18:23:31 -0400

    work in progress

--- futuregreen/urls.py
+++ futuregreen/urls.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 from django.contrib import admin
