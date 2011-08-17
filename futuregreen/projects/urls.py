490548fe9eecaa335919c7058999bfea06d1d3b9
Douglas Meehan <dmeehan@gmail.com>
2011-08-17 17:44:08 -0400

    work in progress

--- futuregreen/projects/urls.py
+++ futuregreen/projects/urls.py
@@ -7,10 +7,10 @@ from django.db.models import get_model
 from futuregreen.projects.views import *
 
 urlpatterns = patterns('',
-   url(r'^projects/$', ProjectListView.as_view(), name = 'projects_project_list'),
-   url(r'^projects/completed/$', ProjectCompletedListView.as_view(), name = 'projects_project_completed'),
-   url(r'^projects/current/$', ProjectCurrentListView.as_view(), name = 'projects_project_current'),
-   url(r'^projects/scale/asc/$', ProjectSizeAscListView.as_view(), name = 'projects_project_size_asc'),
-   url(r'^projects/scale/desc/$', ProjectSizeDescListView.as_view(), name = 'projects_project_size_desc'),
-   url(r'^projects/project/(?P<slug>[-\w]+)/$', ProjectDetailView.as_view(), name = 'projects_project_detail'),
+   url(r'^$', ProjectListView.as_view(), name = 'projects_project_list'),
+   url(r'^completed/$', ProjectCompletedListView.as_view(), name = 'projects_project_completed'),
+   url(r'^current/$', ProjectCurrentListView.as_view(), name = 'projects_project_current'),
+   url(r'^scale/asc/$', ProjectSizeAscListView.as_view(), name = 'projects_project_size_asc'),
+   url(r'^scale/desc/$', ProjectSizeDescListView.as_view(), name = 'projects_project_size_desc'),
+   url(r'^project/(?P<slug>[-\w]+)/$', ProjectDetailView.as_view(), name = 'projects_project_detail'),
 )
