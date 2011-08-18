05e45edeb086c287e0a4b5ad13c37f6721cd3c21
Douglas Meehan <dmeehan@gmail.com>
2011-08-18 11:41:03 -0400

    work in progress

--- futuregreen/dashboard.py
+++ futuregreen/dashboard.py
@@ -29,7 +29,7 @@ class CustomIndexDashboard(Dashboard):
             children=[
                 modules.ModelList(
                     title='Portfolio',
-                    models=('futuregreen.projects.*',)
+                    models=('futuregreen.projects.*', 'easy_maps.*')
                 ),
                 modules.ModelList(
                     title='Studio',
