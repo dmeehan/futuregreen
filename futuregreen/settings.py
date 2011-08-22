f12ede522c6f632c214b4d0dfad6035f70f06fbd
Douglas Meehan <dmeehan@gmail.com>
2011-08-22 10:16:08 -0400

    wip

--- futuregreen/settings.py
+++ futuregreen/settings.py
@@ -66,6 +66,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.request",
     "django.core.context_processors.i18n",
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
     'django.contrib.messages.context_processors.messages',
 )
 
