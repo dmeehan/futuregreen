a93aac3b772d2f05f408a7db22934c066609a975
Douglas Meehan <dmeehan@gmail.com>
2011-08-23 19:03:29 -0400

    wip

--- futuregreen/contacts/admin.py
+++ futuregreen/contacts/admin.py
@@ -7,7 +7,11 @@ from futuregreen.contacts.models import *
 class ContactAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
+class EmployeeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ('name', 'employee_type', 'title')
+
 admin.site.register(Client, ContactAdmin)
 admin.site.register(Collaborator, ContactAdmin)
-admin.site.register(Employee, ContactAdmin)
+admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(EmployeeType, ContactAdmin)
