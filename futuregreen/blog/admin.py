2c78b8d411ca64de5e011c2b06e1c73677368f19
Douglas Meehan <dmeehan@gmail.com>
2011-08-25 11:47:38 -0400

    wip

--- futuregreen/blog/admin.py
+++ futuregreen/blog/admin.py
@@ -1,1 +1,10 @@
 # blog/admin.py
+
+from django.contrib import admin
+
+from blog.models import BlogEntry, BlogLink, BlogImage, LinkImage
+
+admin.site.register(BlogEntry)
+admin.site.register(BlogLink)
+admin.site.register(BlogImage)
+admin.site.register(LinkImage)
