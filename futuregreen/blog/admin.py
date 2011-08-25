bc4bce46c96a0822cc8561db8c860713c9d84d95
Douglas Meehan <dmeehan@gmail.com>
2011-08-25 11:50:08 -0400

    wip

--- futuregreen/blog/admin.py
+++ futuregreen/blog/admin.py
@@ -4,7 +4,24 @@ from django.contrib import admin
 
 from blog.models import BlogEntry, BlogLink, BlogImage, LinkImage
 
-admin.site.register(BlogEntry)
+class ImageInline(admin.StackedInline):
+    model = BlogImage
+    prepopulated_fields = {"slug": ("name",)}
+    fields = ('image', 'is_main', 'name', 'caption',
+              'crop_horz', 'crop_vert', 'slug', 'order' )
+    extra = 0
+
+    # Grappelli options
+    allow_add = True
+    sortable_field_name = "order"
+
+class BlogEntryAdmin(admin.ModelAdmin):
+    inlines = [
+       ImageInline,
+    ]
+
+
+admin.site.register(BlogEntry, BlogEntryAdmin)
 admin.site.register(BlogLink)
 admin.site.register(BlogImage)
 admin.site.register(LinkImage)
