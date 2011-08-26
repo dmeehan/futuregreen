815dfd7dd401e21fc17e37766c93158aba0f990c
Douglas Meehan <dmeehan@gmail.com>
2011-08-26 15:50:32 -0400

    wip

--- futuregreen/urls.py
+++ futuregreen/urls.py
@@ -12,6 +12,8 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     (r'^grappelli/', include('grappelli.urls')),
+    
+    (r'^$', TemplateView.as_view(template_name="index.html")),
 
     # portfolio
     (r'^portfolio/', include('futuregreen.portfolio.urls')),
