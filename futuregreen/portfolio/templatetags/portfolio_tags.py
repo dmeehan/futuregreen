1865fd5aa2a2c9e24e3ad13aaa2d1f4ca1a0de47
Douglas Meehan <dmeehan@gmail.com>
2011-08-26 16:18:46 -0400

    added generic flatblocks to dashboard

---
+++ futuregreen/portfolio/templatetags/portfolio_tags.py
@@ -0,0 +1,15 @@
+# portfolio/templatetags/porfolio_tags.py
+
+from django import template
+from futuregreen.portfolio.models import Project
+
+def compose_featured(parser, token):
+    return FeaturedNode()
+
+class FeaturedNode(template.Node):
+    def render(self, context):
+        context['featured_project_list'] = Project.objects.featured()
+        return ''
+		
+register = template.Library()
+register.tag('get_featured_projects', compose_featured)
