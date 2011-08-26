999771f440531cba9cd62a00c5ddb72a116219a2
Douglas Meehan <dmeehan@gmail.com>
2011-08-26 15:41:40 -0400

    wip

--- futuregreen/portfolio/urls.py
+++ futuregreen/portfolio/urls.py
@@ -6,10 +6,10 @@ from django.conf.urls.defaults import *
 from futuregreen.portfolio.views import *
 
 urlpatterns = patterns('',
-   url(r'^projects/$', ProjectListView.as_view(), name='projects_project_list'),
-   url(r'^projects/completed/$', ProjectCompletedListView.as_view(), name='projects_project_completed'),
-   url(r'^projects/current/$', ProjectCurrentListView.as_view(), name = 'projects_project_current'),
-   url(r'^projects/scale/ascending/$', ProjectSizeAscListView.as_view(), name = 'projects_project_area_asc'),
-   url(r'^projects/scale/desending/$', ProjectSizeDescListView.as_view(), name = 'projects_project_area_desc'),
-   url(r'^project/(?P<slug>[-\w]+)/$', ProjectDetailView.as_view(), name = 'projects_project_detail'),
+   url(r'^projects/$', ProjectListView.as_view(), name='portfolio_project_list'),
+   url(r'^projects/completed/$', ProjectCompletedListView.as_view(), name='portfolio_project_completed'),
+   url(r'^projects/current/$', ProjectCurrentListView.as_view(), name = 'portfolio_project_current'),
+   url(r'^projects/scale/ascending/$', ProjectSizeAscListView.as_view(), name = 'portfolio_project_area_asc'),
+   url(r'^projects/scale/desending/$', ProjectSizeDescListView.as_view(), name = 'portfolio_project_area_desc'),
+   url(r'^project/(?P<slug>[-\w]+)/$', ProjectDetailView.as_view(), name = 'portfolio_project_detail'),
 )
