d9e209ddb234737a506a7c1405635828d6db49b4
Douglas Meehan <dmeehan@gmail.com>
2011-08-29 16:25:08 -0400

    edited image inline for projects

--- futuregreen/portfolio/admin.py
+++ futuregreen/portfolio/admin.py
@@ -18,7 +18,7 @@ class ImageInline(admin.StackedInline):
     model = ProjectImage
     fieldsets = (
         ('Image', {
-            'fields': ('image', 'name', 'description',)
+            'fields': ('image', 'name', 'description',),
         }),
         ('Options', {
             'classes': ('collapse closed',),
