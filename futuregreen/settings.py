08bc3802373de4d05d5f37dbaee1f35d9d974cc9
Douglas Meehan <dmeehan@gmail.com>
2011-08-29 12:35:41 -0400

    debug = false

--- futuregreen/settings.py
+++ futuregreen/settings.py
@@ -6,7 +6,7 @@ PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 SITE_ROOT = os.path.join(PROJECT_DIR, os.pardir)
 PROJECT_URL = '/'
 
-DEBUG = FALSE
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
