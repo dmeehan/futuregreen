86271a512da89cb2bb175aea58d3c2fafce267a1
Douglas Meehan <dmeehan@gmail.com>
2011-09-01 17:24:02 -0400

    added new views and urls for landscape type and project type

--- futuregreen/portfolio/urls.py
+++ futuregreen/portfolio/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     url(r'^projects/scale/descending/$', ProjectSizeDescListView.as_view(), name = 'portfolio_project_area_desc'),
     url(r'^project/(?P<slug>[-\w]+)/$', ProjectDetailView.as_view(), name = 'portfolio_project_detail'),
 
-    url(r'^projects/projecttypes/$', ListView.as_view(queryset = ProjectType.filter(level=0)),
+    url(r'^projects/projecttypes/$', ListView.as_view(queryset = ProjectType._default_manager.filter(level=0)),
         name = 'categories_tree_list'),
     url(r'^projects/projecttypes/(?P<path>.+)/$', CategoryDetailView.as_view(model=ProjectType,
         template_name = 'portfolio/projects_by_type.html'), name = 'portfolio_projects_by_type'),
