1913ac1eb6d6ffa83309596a47efc16c2798020f
Douglas Meehan <dmeehan@gmail.com>
2011-09-01 14:18:54 -0400

    added new views and urls for landscape type and project type

--- futuregreen/portfolio/urls.py
+++ futuregreen/portfolio/urls.py
@@ -8,10 +8,15 @@ from categories.views import CategoryDetailView, CategoryRelatedList,
 from futuregreen.portfolio.views import *
 
 urlpatterns = patterns('',
-   url(r'^projects/$', ProjectListView.as_view(), name='portfolio_project_list'),
-   url(r'^projects/completed/$', ProjectCompletedListView.as_view(), name='portfolio_project_completed'),
-   url(r'^projects/current/$', ProjectCurrentListView.as_view(), name = 'portfolio_project_current'),
-   url(r'^projects/scale/ascending/$', ProjectSizeAscListView.as_view(), name = 'portfolio_project_area_asc'),
-   url(r'^projects/scale/descending/$', ProjectSizeDescListView.as_view(), name = 'portfolio_project_area_desc'),
-   url(r'^project/(?P<slug>[-\w]+)/$', ProjectDetailView.as_view(), name = 'portfolio_project_detail'),
+    url(r'^projects/$', ProjectListView.as_view(), name='portfolio_project_list'),
+    url(r'^projects/completed/$', ProjectCompletedListView.as_view(), name='portfolio_project_completed'),
+    url(r'^projects/current/$', ProjectCurrentListView.as_view(), name = 'portfolio_project_current'),
+    url(r'^projects/scale/ascending/$', ProjectSizeAscListView.as_view(), name = 'portfolio_project_area_asc'),
+    url(r'^projects/scale/descending/$', ProjectSizeDescListView.as_view(), name = 'portfolio_project_area_desc'),
+    url(r'^project/(?P<slug>[-\w]+)/$', ProjectDetailView.as_view(), name = 'portfolio_project_detail'),
+)
+
+urlpatterns += patterns('',
+    (r'^projects/categories/', include('categories.urls')),
+
 )
