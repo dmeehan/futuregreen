b7f0b3b30e7271359ba5505e1a91e7fc13584074
Douglas Meehan <dmeehan@gmail.com>
2011-09-20 17:23:45 -0400

    editing homepage

--- futuregreen/urls.py
+++ futuregreen/urls.py
@@ -4,18 +4,13 @@ from django.views.generic import TemplateView, ListView, DetailView
 from django.contrib import admin
 admin.autodiscover()
 
-from futuregreen.portfolio.models import Project
-
 urlpatterns = patterns('',
     # admin
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     (r'^grappelli/', include('grappelli.urls')),
     
-    url(r'^$', ListView.as_view(context_object_name = "project_list",
-        queryset = Project._default_manager.live().filter(featured=True).order_by('?')[:10],
-        template_name = "index.html",),
-        name = 'home'),
+    url(r'^$', 'futuregreen.views.index',name = 'home'),
 
     # portfolio
     (r'^portfolio/', include('futuregreen.portfolio.urls')),
--- futuregreen/views.py
+++ futuregreen/views.py
@@ -1,1 +1,9 @@
 # futuregreen/views.py
+from django.shortcuts import render
+
+from futuregreen.portfolio.models import Project
+
+def index(request):
+    projects = Project._default_manager.live().filter(featured=True).order_by('?')
+    project_list = list(projects)
+    return render('index.html', {'project_list': project_list})
