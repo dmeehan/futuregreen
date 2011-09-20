cd1060fe94fece6a4ce353aa63b6b4ae66f570ac
Douglas Meehan <dmeehan@gmail.com>
2011-09-20 14:50:49 -0400

    editing homepage

--- futuregreen/urls.py
+++ futuregreen/urls.py
@@ -10,7 +10,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     (r'^grappelli/', include('grappelli.urls')),
     
-    url(r'^$', TemplateView.as_view(template_name="index.html"), name = 'home'),
+    url(r'^$', ListView.as_view(context_object_name = "project_list",
+        queryset = Project.objects.filter(featured=True).order_by('?')),
+        name = 'home'),
 
     # portfolio
     (r'^portfolio/', include('futuregreen.portfolio.urls')),
