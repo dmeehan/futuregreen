7780c5634ea2b795aa588e9d817957ebe0b75c9e
Douglas Meehan <dmeehan@gmail.com>
2011-09-20 16:31:42 -0400

    editing homepage

--- futuregreen/views.py
+++ futuregreen/views.py
@@ -1,10 +1,1 @@
 # futuregreen/views.py
-from django.views.generic import ListView
-from futuregreen.portfolio.models import Project
-
-class IndexView(ListView):
-    """
-        Get the featured projects
-    """
-    context_object_name = "project_list"
-    queryset = Project._default_manager.live().filter(featured=True).order_by("-date_published")
