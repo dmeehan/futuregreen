1ed4a93a12a038768bf3f41208aabe682bdb63f8
Douglas Meehan <dmeehan@gmail.com>
2011-09-23 11:14:58 -0400

    editing homepage

--- futuregreen/views.py
+++ futuregreen/views.py
@@ -6,6 +6,6 @@ from futuregreen.news.models import Item
 def index(request):
     projects = Project._default_manager.live().filter(featured=True).order_by('?')
     project_list = list(projects)
-    item = Item._default_manager.live().latest()
+    item = Item._default_manager.latest()
     return render(request, 'index.html', {'project_list': project_list,
                                           'item': item })
