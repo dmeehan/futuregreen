c14f9ed1f3c145c844bf49ca2d161d6c7306711b
Douglas Meehan <dmeehan@gmail.com>
2011-09-23 11:12:54 -0400

    editing homepage

--- futuregreen/views.py
+++ futuregreen/views.py
@@ -2,8 +2,10 @@
 from django.shortcuts import render
 
 from futuregreen.portfolio.models import Project
-
+from futuregreen.news.models import Item
 def index(request):
     projects = Project._default_manager.live().filter(featured=True).order_by('?')
     project_list = list(projects)
-    return render(request, 'index.html', {'project_list': project_list})
+    item = Item._default_manager.live().latest()
+    return render(request, 'index.html', {'project_list': project_list,
+                                          'item': item })
