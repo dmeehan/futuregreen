7350a0bbbaaf201f7b6460468faa3f574d9cb93d
Douglas Meehan <dmeehan@gmail.com>
2011-10-25 09:43:56 -0400

    working on studio section of site

--- futuregreen/studio/urls.py
+++ futuregreen/studio/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.defaults import *
 from django.views.generic import ListView, DetailView, TemplateView
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="studio_index.html"), name='studio'),
+    url(r'^$', TemplateView.as_view(template_name="studio/studio_index.html"), name='studio'),
 )
