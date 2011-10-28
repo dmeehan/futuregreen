bf330fd71162c15aadeabf4fb49981b7ded4155a
Douglas Meehan <dmeehan@gmail.com>
2011-10-28 11:51:12 -0400

    cleaning up homepage layout

--- futuregreen/studio/urls.py
+++ futuregreen/studio/urls.py
@@ -6,5 +6,5 @@ from django.views.generic import ListView, DetailView, TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="studio/studio_index.html"), name='studio'),
-    url(r'^$', TemplateView.as_view(template_name="studio/studio_news.html"), name='studio_news'),
+    url(r'^news/$', TemplateView.as_view(template_name="studio/studio_news.html"), name='studio_news'),
 )
