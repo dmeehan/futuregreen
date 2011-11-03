1e30a1b4cc031dbaf79e73a6d636656cfc33b460
Douglas Meehan <dmeehan@gmail.com>
2011-11-03 14:05:20 -0400

    studio section

--- futuregreen/studio/views.py
+++ futuregreen/studio/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 
 from futuregreen.people.models import Employee
 from futuregreen.news.models import Item
+from futuregreen.portfolio.models import Project
 
 def index(request):
     news = Item._default_manager.latest('date_published')
     employee_list = Employee._default_manager.filter(status=Employee.STATUS_FULL)
+    project_list = Project._default_manager.live().filter(featured=True).order_by('?')[:4]
     return render(request, 'studio/studio_index.html', {'employee_list': employee_list,
-                                          'news': news })
+                                                        'project_list': project_list,
+                                                        'news': news })
