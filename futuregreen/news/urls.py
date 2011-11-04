76221603e15b14fb20e84a4b2a2ff5e18ad54f4c
Douglas Meehan <dmeehan@gmail.com>
2011-11-04 17:03:43 -0400

    editing site nav styles

--- futuregreen/news/urls.py
+++ futuregreen/news/urls.py
@@ -2,10 +2,13 @@
 
 from django.conf import settings
 from django.conf.urls.defaults import *
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView, DetailView, TemplateView, DateDetailView
 
 from futuregreen.news.models import Item
 
 urlpatterns = patterns('',
-    url(r'^$', ListView.as_view(model=Item, context_object_name = "news_list"), name = 'news_list'),
+    url(r'^$', ListView.as_view(model=Item, context_object_name = "news_list"), name = 'news_item_list'),
+    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
+        DateDetailView.as_view(model=Item, context_object_name = "item"),
+        name = 'news_item_detail'),
 )
