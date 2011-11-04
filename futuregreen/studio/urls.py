c026970f7a5f5e8e6af8492c35796e8aabbb382e
Douglas Meehan <dmeehan@gmail.com>
2011-11-04 16:25:22 -0400

    editing site nav styles

--- futuregreen/studio/urls.py
+++ futuregreen/studio/urls.py
@@ -6,7 +6,6 @@ from django.views.generic import ListView, DetailView, TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', 'futuregreen.studio.views.index', name = 'studio'),
-    url(r'^news/$', TemplateView.as_view(template_name="studio/studio_news.html"), name='studio_news'),
     (r'^people/', include('futuregreen.people.urls')),
     (r'^news/', include('futuregreen.news.urls')),
 )
