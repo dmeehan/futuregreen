266cef62c0df8cb21e5f126f2a27dd9cafa43068
Douglas Meehan <dmeehan@gmail.com>
2011-11-04 11:52:57 -0400

    editing categories css

--- futuregreen/portfolio/views.py
+++ futuregreen/portfolio/views.py
@@ -16,6 +16,14 @@ class ProjectListView(ListView):
     queryset = Project._default_manager.live()
     paginate_by = settings.PROJECT_PAGINATE_BY
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ProjectListView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the categories
+        context['projecttype_list'] = ProjectType._default_manager.all()
+        context['landscapetype_list'] = LandscaoeType._default_manager.all()
+        return context
+
 class ProjectSizeAscListView(ProjectListView):
     queryset = Project._default_manager.size_asc()
 
