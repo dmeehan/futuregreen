b24882c0e741e7d294f53264a413b776b031b0e2
Douglas Meehan <dmeehan@gmail.com>
2011-11-04 17:06:56 -0400

    editing site nav styles

--- futuregreen/news/urls.py
+++ futuregreen/news/urls.py
@@ -9,6 +9,6 @@ from futuregreen.news.models import Item
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=Item, context_object_name = "news_list"), name = 'news_item_list'),
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
-        DateDetailView.as_view(model=Item, context_object_name = "item"),
+        DateDetailView.as_view(model=Item, context_object_name = "item", date_field="date_published"),
         name = 'news_item_detail'),
 )
