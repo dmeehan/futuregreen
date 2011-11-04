c95ecf4c0ef62db7b4d4e5d83690d9699765a766
Douglas Meehan <dmeehan@gmail.com>
2011-11-04 09:53:42 -0400

    editing footer

--- futuregreen/studio/views.py
+++ futuregreen/studio/views.py
@@ -7,9 +7,9 @@ from futuregreen.portfolio.models import Project
 
 def index(request):
     news = Item._default_manager.latest('date_published')
-    principal_list = Employee._default_manager.filter(employee_type='Principal')
-    associate_list = Employee._default_manager.filter(employee_type='Associate')
-    staff_list = Employee._default_manager.filter(employee_type='Staff')
+    principal_list = Employee._default_manager.filter(employee_type__name='Principal')
+    associate_list = Employee._default_manager.filter(employee_type__name='Associate')
+    staff_list = Employee._default_manager.filter(employee_type__name='Staff')
     project_list = Project._default_manager.live().filter(featured=True).order_by('?')[:1]
     return render(request, 'studio/studio_index.html', {'principal_list': principal_list,
                                                         'associate_list': associate_list,
