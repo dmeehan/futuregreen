97e92bb1444e3e54be03788819a51b5200463cce
Douglas Meehan <dmeehan@gmail.com>
2011-11-06 13:46:25 -0500

    editing homepage styles

--- futuregreen/people/urls.py
+++ futuregreen/people/urls.py
@@ -8,5 +8,5 @@ from futuregreen.people.views import EmployeeDetailView, EmployeeListView
 
 urlpatterns = patterns('',
     url(r'^$', EmployeeListView.as_view(), name = 'person_list'),
-    url(r'^(?P<slug>[-\w]+)/$', EmployeeDetailView.as_view(), name = 'person_detail'),
+    url(r'^(?P<slug>[-\w]+)/$', EmployeeDetailView.as_view(), name = 'people_person_detail'),
 )
