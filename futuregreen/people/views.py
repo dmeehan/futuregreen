4af8c37ca49fd18ba27ba26961af9166c618b487
Douglas Meehan <dmeehan@gmail.com>
2011-11-06 14:15:57 -0500

    editing homepage styles

--- futuregreen/people/views.py
+++ futuregreen/people/views.py
@@ -7,5 +7,14 @@ from futuregreen.people.models import *
 class EmployeeDetailView(DetailView):
     model = Employee
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(EmployeeDetailView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the categories
+        context['employee_list'] = Employee._default_manager.exclude(employee_status=Employee.STATUS_FORMER)\
+                                                            .exclude(employee_slug==self.args[0])\
+                                                            .order_by(employee_type)
+        return context
+
 class EmployeeListView(ListView):
     model = Employee
