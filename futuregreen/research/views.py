f15d288b1193b4322bc829cee8e6587e2970353d
Douglas Meehan <dmeehan@gmail.com>
2011-11-07 14:55:46 -0500

    editing site nav styles

--- futuregreen/research/views.py
+++ futuregreen/research/views.py
@@ -7,6 +7,6 @@ from futuregreen.portfolio.models import Project
 
 def index(request):
     entry = Entry._default_manager.latest('date_published')
-    project_list = Project._default_manager.live().filter(project_type__name='Research')
+    project_list = Project._default_manager.live().filter(project_types__name='Research')
     return render(request, 'studio/studio_index.html', {'project_list': project_list,
                                                         'entry': entry })
