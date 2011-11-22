6ce828108b1d688ac46d31dff15cf794fbf1685a
Douglas Meehan <dmeehan@gmail.com>
2011-11-22 11:33:24 -0500

    refactoring local apps

--- futuregreen/research/templatetags/article_tags.py
+++ futuregreen/research/templatetags/article_tags.py
@@ -6,37 +6,37 @@ from django import template
 from django.conf import settings
 from django.db import models
 
-Entry = models.get_model('research', 'entry')
+Article = models.get_model('research', 'article')
 
 register = template.Library()
 
 
-class LatestEntries(template.Node):
+class LatestArticles(template.Node):
     def __init__(self, limit, var_name):
         self.limit = int(limit)
         self.var_name = var_name
 
     def render(self, context):
-        entries = Entry._default_manager.live()[:self.limit]
-        if entries and (self.limit == 1):
-            context[self.var_name] = entries[0]
+        articles = Article._default_manager.live()[:self.limit]
+        if articles and (self.limit == 1):
+            context[self.var_name] = articles[0]
         else:
-            context[self.var_name] = entries
+            context[self.var_name] = articles
         return ''
 
 
 @register.tag
-def get_latest_entries(parser, token):
+def get_latest_articles(parser, token):
     """
-Gets any number of latest posts and stores them in a varable.
+Gets any number of latest articles and stores them in a variable.
 
 Syntax::
 
-{% get_latest_posts [limit] as [var_name] %}
+{% get_latest_articles [limit] as [var_name] %}
 
 Example usage::
 
-{% get_latest_posts 10 as latest_post_list %}
+{% get_latest_articles 10 as latest_article_list %}
 """
     try:
         tag_name, arg = token.contents.split(None, 1)
@@ -46,4 +46,4 @@ Example usage::
     if not m:
         raise template.TemplateSyntaxError, "%s tag had invalid arguments" % tag_name
     format_string, var_name = m.groups()
-    return LatestEntries(format_string, var_name)
+    return Latestarticles(format_string, var_name)
