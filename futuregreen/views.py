26512dd7b4928205f754e4a74482a028f4e985f3
Douglas Meehan <dmeehan@gmail.com>
2011-11-22 10:35:42 -0500

    refactoring local apps

--- futuregreen/views.py
+++ futuregreen/views.py
@@ -3,11 +3,11 @@
 from django.shortcuts import render
 
 from futuregreen.portfolio.models import Project
-from futuregreen.news.models import NewsItem
+from futuregreen.studio.models import NewsItem
 
 def index(request):
     projects = Project._default_manager.live().filter(featured=True).order_by('?')
     project_list = list(projects)
-    news = Newsitem._default_manager.latest('date_published')
+    news = NewsItem._default_manager.latest('date_published')
     return render(request, 'index.html', {'project_list': project_list,
                                           'news': news })
