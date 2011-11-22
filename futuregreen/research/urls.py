c6168463ee78e0ab0630bb456136f55734d99e94
Douglas Meehan <dmeehan@gmail.com>
2011-11-22 10:45:49 -0500

    refactoring local apps

--- futuregreen/research/urls.py
+++ futuregreen/research/urls.py
@@ -6,6 +6,4 @@ from django.views.generic import ListView, DetailView, TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', 'futuregreen.research.views.index', name = 'research'),
-    (r'^research/', include('futuregreen.research.urls')),
-    (r'^news/', include('futuregreen.news.urls')),
 )
