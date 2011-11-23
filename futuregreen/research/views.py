37cc8b2ccf257195a1f574f31fb9f57eddce8c1e
Douglas Meehan <dmeehan@gmail.com>
2011-11-23 11:44:08 -0500

    editing css

--- futuregreen/research/views.py
+++ futuregreen/research/views.py
@@ -17,5 +17,5 @@ class ArticleIndexView(ArchiveIndexView):
         # Call the base implementation first to get a context
         context = super(ArticleIndexView, self).get_context_data(**kwargs)
         # Add in a QuerySet of all the projects in the research category
-        context['project_list'] = Project._default_manager.live().filter(project_types__name='Research')
+        context['project_list'] = Project._default_manager.filter(project_types__name='Research', status=Project.STATUS_LIVE)
         return context
