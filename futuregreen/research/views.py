4a2bda6fc4ccad43177b6072134ce55dd520eb09
Douglas Meehan <dmeehan@gmail.com>
2011-11-23 11:39:53 -0500

    editing css

--- futuregreen/research/views.py
+++ futuregreen/research/views.py
@@ -3,11 +3,19 @@
 from django.views.generic import ArchiveIndexView, DateDetailView
 
 from futuregreen.research.models import Article
+from futuregreen.portfolio.models import Project
 
-class ArticleyDetailView(DateDetailView):
+class ArticleDetailView(DateDetailView):
     queryset = Article._default_manager.live()
     date_field="date_published"
 
 class ArticleIndexView(ArchiveIndexView):
     queryset = Article._default_manager.live()
     date_field="date_published"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ArticleIndexView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the categories
+        context['project_list'] = Project._default_manager.live().filter(project_types__name='research')
+        return context
