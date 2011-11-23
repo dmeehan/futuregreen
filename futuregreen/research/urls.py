a1d2f9a497dda959e1431350ecfb1da9e1c9b675
Douglas Meehan <dmeehan@gmail.com>
2011-11-23 12:03:31 -0500

    editing css

--- futuregreen/research/urls.py
+++ futuregreen/research/urls.py
@@ -4,8 +4,10 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 from django.views.generic import ListView, DetailView, TemplateView
 
-from futuregreen.research.views import ArticleIndexView
+from futuregreen.research.views import ArticleIndexView, ArticleDetailView
 
 urlpatterns = patterns('',
     url(r'^$', ArticleIndexView.as_view() , name = 'research'),
+    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
+        ArticleDetailView.as_view(), name = 'research_article_detail'),
 )
