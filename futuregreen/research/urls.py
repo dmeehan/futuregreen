b49e99d1a5b121151335587e1eba04f07f008f4e
Douglas Meehan <dmeehan@gmail.com>
2011-11-29 13:03:32 -0500

    editing css

--- futuregreen/research/urls.py
+++ futuregreen/research/urls.py
@@ -6,8 +6,6 @@ from django.views.generic import YearArchiveView, MonthArchiveView,\
     WeekArchiveView, DayArchiveView, TodayArchiveView, \
     DetailView, ListView, TemplateView
 
-from taggit.views import tagged_object_list
-
 from futuregreen.research.models import Article
 from futuregreen.research.views import ArticleIndexView, ArticleDetailView
 
@@ -33,10 +31,10 @@ urlpatterns = patterns('',
         date_field="date_published"),
         name='article_archive_year'
     ),
-    url(r'^tag/(?P<slug>[-\w]+)/$',
-        tagged_object_list(queryset=Article._default_manager.live,
-                           template_name='articles_by_tag.html',
-                           template_object_name='article'),
+    url(r'^tag/(?P<slug>[-\w]+)/$','taggit.views.tagged_object_list',
+        {'queryset': Article._default_manager.live(),
+         'template_name': 'articles_by_tag.html',
+         'template_object_name': 'article'},
         name='articles_by_tag'
     ),
 )
