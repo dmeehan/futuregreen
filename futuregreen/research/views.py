a7074fcaf5ce42d52b25e2282887c03bbc296a86
Douglas Meehan <dmeehan@gmail.com>
2011-11-29 14:41:22 -0500

    editing article views

--- futuregreen/research/views.py
+++ futuregreen/research/views.py
@@ -1,26 +1,39 @@
 # research/views.py
 
-from django.views.generic import View, ArchiveIndexView, DateDetailView, MonthArchiveView
+from django.views.generic import ArchiveIndexView, DateDetailView, MonthArchiveView
 
 from futuregreen.research.models import Article
 from futuregreen.portfolio.models import Project
 
-class ArticleViewMixin(View):
+class ArticleDetailView(DateDetailView):
     queryset = Article._default_manager.live()
     date_field="date_published"
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
-        context = super(ArticleViewMixin, self).get_context_data(**kwargs)
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
         # Add in a QuerySet of all the projects in the research category
         context['project_list'] = Project._default_manager.live().filter(project_types__name='Research')
         return context
 
-class ArticleDetailView(DateDetailView, ArticleViewMixin):
-    pass
+class ArticleIndexView(ArchiveIndexView):
+    queryset = Article._default_manager.live()
+    date_field="date_published"
 
-class ArticleIndexView(ArchiveIndexView, ArticleViewMixin):
-    pass
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ArticleIndexView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the projects in the research category
+        context['project_list'] = Project._default_manager.live().filter(project_types__name='Research')
+        return context
 
-class ArticleMonthArchiveView(MonthArchiveView, ArticleViewMixin):
-    pass
+class ArticleMonthArchiveView(MonthArchiveView):
+    queryset = Article._default_manager.live()
+    date_field="date_published"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ArticleMonthArchiveView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the projects in the research category
+        context['project_list'] = Project._default_manager.live().filter(project_types__name='Research')
+        return context
