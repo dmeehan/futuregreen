a0bd8a1e7dac408c2200499d378680ea594fbf90
Douglas Meehan <dmeehan@gmail.com>
2011-11-29 14:44:26 -0500

    editing article views

--- futuregreen/research/urls.py
+++ futuregreen/research/urls.py
@@ -11,24 +11,29 @@ from futuregreen.research.views import ArticleIndexView, ArticleDetailView, Arti
 
 urlpatterns = patterns('',
     url(r'^$', ArticleIndexView.as_view() , name = 'research'),
+
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
         ArticleDetailView.as_view(), name = 'research_article_detail'),
-     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/$',
+
+    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/$',
         DayArchiveView.as_view(
         queryset=Article._default_manager.live(),
         date_field="date_published"),
         name='article_archive_day'
     ),
+
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/$',
         ArticleMonthArchiveView.as_view(),
         name='article_archive_month'
     ),
+
     url(r'^(?P<year>\d{4})/$',
         YearArchiveView.as_view(
         queryset=Article._default_manager.live(),
         date_field="date_published"),
         name='article_archive_year'
     ),
+    
     url(r'^tag/(?P<slug>[-\w]+)/$','taggit.views.tagged_object_list',
         {'queryset': Article._default_manager.live(),
          'template_name': 'research/articles_by_tag.html',
