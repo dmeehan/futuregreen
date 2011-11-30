6eeee5839e98154107ec1864023b617728b9ae15
Douglas Meehan <dmeehan@gmail.com>
2011-11-30 09:14:45 -0500

    editing css

--- futuregreen/views.py
+++ futuregreen/views.py
@@ -4,10 +4,13 @@ from django.shortcuts import render
 
 from futuregreen.portfolio.models import Project
 from futuregreen.studio.models import NewsItem
+from futuregreen.research.models import Article
 
 def index(request):
     projects = Project._default_manager.live().filter(featured=True).order_by('?')
     project_list = list(projects)
     news = NewsItem._default_manager.latest('date_published')
+    article = Article._default_manager.latest('date_published')
     return render(request, 'index.html', {'project_list': project_list,
-                                          'news': news })
+                                          'news': news,
+                                          'article': article })
