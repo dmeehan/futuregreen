180de794cb5c871e3f021fd9b9d2b8e4a3f6f6cc
Douglas Meehan <dmeehan@gmail.com>
2011-12-02 14:39:37 -0500

    editing templates

--- futuregreen/studio/models.py
+++ futuregreen/studio/models.py
@@ -1,4 +1,5 @@
 # studio/models.py
+import os
 
 from django.conf import settings
 from django.db import models
@@ -41,9 +42,15 @@ class NewsItemFile(models.Model):
     file = models.FileField(upload_to='files/news/', blank=True)
     news_item = models.ForeignKey(NewsItem)
 
+    def __unicode__(self):
+        return u'%s' % os.path.basename(self.file.name)
+
 class NewsItemProject(models.Model):
     """
         A project related to the article
     """
     project = models.ForeignKey(Project)
     article = models.ForeignKey(NewsItem)
+
+    def __unicode__(self):
+        return u'%s' % self.project.name
