9aa2275a5c303b6b14757d2b1e94da1e6988018d
Douglas Meehan <dmeehan@gmail.com>
2011-12-02 15:44:58 -0500

    editing templates

--- futuregreen/settings.py
+++ futuregreen/settings.py
@@ -182,6 +182,8 @@ EASY_MAPS_GOOGLE_KEY = 'ABQIAAAAayJegR1S7-F1AMio1LsppBSLB-iwKszotXgLQy-KoofPut07
 
 PIL_IMAGEFILE_MAXBLOCK = 1024 * 2 ** 10
 
+COMPRESS_ENABLED = True
+
 #==============================================================================
 # local app settings
 #==============================================================================
