b705e6dccd4d0c6ef016aa7bfab5ebc6bb47cea5
dmeehan <dmeehan@gmail.com>
2012-11-03 02:25:06 +0000

    moved news to its own application and to a top level navigation item

--- futuregreen/studio/views.py
+++ futuregreen/studio/views.py
@@ -12,10 +12,9 @@ def index(request):
     principal_list = Employee._default_manager.filter(public=True, employee_type__name='Principal')
     associate_list = Employee._default_manager.filter(public=True, employee_type__name='Associate')
     staff_list = Employee._default_manager.filter(public=True, employee_type__name='Staff')
+    employee_list = Employee._default_manager.filter(public=True).order_by('employee_type__level')
     project_list = Project._default_manager.live().filter(featured=True).order_by('?')[:1]
-    return render(request, 'studio/studio_index.html', {'principal_list': principal_list,
-                                                        'associate_list': associate_list,
-                                                        'staff_list': staff_list,
+    return render(request, 'studio/studio_index.html', {'employee_list': employee_list,
                                                         'project_list': project_list,
                                                         'news': news })
 
