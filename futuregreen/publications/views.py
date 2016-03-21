f47d409a0516d7d0443fb676eb8147c2c3cf2185
Douglas Meehan <dmeehan@gmail.com>
2016-03-21 09:10:07 -0400

    fixed publication views to work with django 1.3

--- futuregreen/publications/views.py
+++ futuregreen/publications/views.py
@@ -8,7 +8,7 @@ class PublicationView(TemplateView):
     template_name = 'book.html'
 
     def get_context_data(self, **kwargs):
-        return {'book': Publication.objects.all[0]() }
+        return {'book': Publication.objects.all()[0] }
 
 
 
