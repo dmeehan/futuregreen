835c455503a75658d8b744df643158ac6575b737
Douglas Meehan <dmeehan@gmail.com>
2017-06-21 09:21:03 -0400

    make sure latest news item and research article are live

--- futuregreen/views.py
+++ futuregreen/views.py
@@ -9,8 +9,8 @@ from futuregreen.research.models import Article
 def index(request):
     projects = Project._default_manager.live().filter(featured=True).order_by('?')
     project_list = list(projects)
-    news = NewsItem._default_manager.latest('date_published')
-    article = Article._default_manager.latest('date_published')
+    news = NewsItem._default_manager.live().latest('date_published')
+    article = Article._default_manager.live().latest('date_published')
     return render(request, 'index.html', {'project_list': project_list,
                                           'news': news,
                                           'article': article })
